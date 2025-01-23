c2e3aac544cc1a30cedff1ef540f109c6849049b
IGoByALotOfNames <helloworld202110@zohomail.com>
2025-01-23 16:52:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-
-url = st.secrets["URL"]
-st.components.v1.iframe(url, width=800, height=600)
+if st.button("Open"):
+    components.html(
+        f'''
+        <script type="text/javascript">
+        window.open("{st.secrets["URL"]}", "_blank").focus();
+        </script>
+        ''',
+        height=300,
+    )
